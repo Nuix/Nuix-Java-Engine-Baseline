a0f3dfc77d74acb0104bf7f5267f2330e0690cb4
Jason Wells <jason.wells@nuix.com>
2019-02-06 10:24:12 -0800

    Update EntryPoint.java
    Reference engine release 7.8.0.10

--- Java/src/main/java/com/nuix/javaenginesimple/EntryPoint.java
+++ Java/src/main/java/com/nuix/javaenginesimple/EntryPoint.java
@@ -23,7 +23,7 @@ public class EntryPoint {
 				
 		// Create an instance of engine wrapper, which will do the work of getting the Nuix bits initialized.
 		// Engine wrapper will need to know what directory you engine release resides.
-		EngineWrapper wrapper = new EngineWrapper("D:\\engine-releases\\7.6.8");
+		EngineWrapper wrapper = new EngineWrapper("D:\\engine-releases\\7.8.0.10");
 		
 		try {
 			// Attempt to initialize Nuix with a dongle based license
